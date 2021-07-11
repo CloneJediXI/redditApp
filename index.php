61cde6c3939e491b4a87bd55d8c257e9b2b1e3ad
Spencer Burke <1spencerburke@gmail.com>
2021-07-11 14:13:01 +0000

    Can add tags to a post

--- index.php
+++ index.php
@@ -12,9 +12,12 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     </head>
-    <body>
+    <body style="max-width: 100%;overflow-x: hidden;">
         <script type="text/javascript">
+            var tags = [];
+            var tagNumbers = [];
             $(document).ready(function(){
+                
                 var $form = $('#newTagForm');
                 $form.submit(function(){
                     $.post($(this).attr('action'), $(this).serialize(), function(response){
@@ -40,13 +43,42 @@
                             location.reload();
                         };
                         window.setTimeout( show_popup, 500 );
+                    }else if (id == "addTagToPost") {
+                        //var tagDrop = $('#tagDrop').text();
+                        //$('#tagsList').val("test");
+                        
                     }
                     
+                });
+                $('#tagDrop').change(function(){
+                    var tag = $('#tagDrop option:selected').text();
+                    var tagNumber = $('#tagDrop').val();
+                    if (tagNumber == "-1"){
+                        tags = [];
+                        tagNumbers = [];
+                    }else if(tagNumbers.includes(tagNumber)){
+                        var index = tagNumbers.indexOf(tagNumber);
+                        if (index >-1){
+                            tagNumbers.splice(index, 1);
+                        }
+                        index = tags.indexOf(tag);
+                        if (index >-1){
+                            tags.splice(index, 1);
+                        }
+                    }else {
+                        tags.push(tag);
+                        tagNumbers.push(tagNumber);
+                    }
+                    //$('#tagsList').text(tags);
+                    $('#tagsIdList').val(tagNumbers);
+                    var tagNames = "";
+                    tags.forEach(element => tagNames+= element+", ");
+                    $('#tagsList').text(tagNames);
                 });
             });
         </script>
         <h1 class="text-center">Welcome to the reddit post saver!</h1>
-        <form action="save.php" method="POST">
+        <form action="save.php" method="POST" id="postSaveForm">
             <div class="row">
                 <div class="col-lg-4 col-1"></div>
                 <div class="col-lg-4 col-10">
@@ -58,15 +90,19 @@
             <div class="row">
                 <div class="col-lg-4 col-1"></div>
                 <div class="col-lg-4 col-10">
-                    <label for="tags">Tags to apply to post</label>
-                    <input name="tags" type="text" disabled />
+                    <label for="tags">Tags to apply to post : </label>
+                    <!--<input id="tagsList" name="tags" type="text" disabled /> -->
+                    <span role="textbox" id="tagsList" name="tags" style="border: 1px solid #ccc;padding: 1px 6px; margin-left: 5px;"></span>
+                    
+                    <input id="tagsIdList" name="tags" type="hidden"/>
                     <br/>
-                    <select name="tagList">
+                    <select name="tagList" id="tagDrop">
                         <?php 
                             $connection = new SQLiteConnection();
                             $pdo = $connection->connect();
                             if ($pdo != null){
                                 $tags = $connection->getAllTags();
+                                echo "<option value=\"-1\">Remove all Tags</option>";
                                 foreach($tags as $tag){
                                     $id = $tag['tag_id'];
                                     $name = $tag['tag_name'];
@@ -75,7 +111,7 @@
                             }
                         ?>
                     </select>
-                    <input type="submit" value="Submit" id="savePostFormSubmit">
+                    <input type="submit" value="Submit" id="savePostFormSubmit" />
                 </div>
             </div>
         </form>
@@ -95,7 +131,7 @@
                         foreach($tags as $tag){
                             $id = $tag['tag_id'];
                             $name = $tag['tag_name'];
-                            echo "<p class=\"text-center\">$id : $name <button id=\"$id\" class='btn btn-danger'> X </button></p>";
+                            echo "<p class=\"text-center\">$id : $name <button id=\"$id\" class='btn btn-outline-danger'> X </button></p>";
                         }
                     }
                 ?>
